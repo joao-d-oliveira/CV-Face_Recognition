be4684fa7264bfb3dcbb43642ab8e84262cd4a92
Joao Oliveira <joao.d.oliveira@gmail.com>
2021-06-02 13:29:02 -0300

    improved rotation

--- workspace_utils.py
+++ workspace_utils.py
@@ -53,6 +53,7 @@ def keep_awake(iterable, delay=DELAY, interval=INTERVAL):
     """
     with active_session(delay, interval): yield from iterable        
         
+# to use only in Collab
 def checkFreeGPU():
     # memory footprint support libraries/code
     !ln -sf /opt/bin/nvidia-smi /usr/bin/nvidia-smi
@@ -73,20 +74,5 @@ def checkFreeGPU():
         print("Gen RAM Free: " + humanize.naturalsize(psutil.virtual_memory().available), " |     Proc size: " + humanize.naturalsize(process.memory_info().rss))
         print("GPU RAM Free: {0:.0f}MB | Used: {1:.0f}MB | Util {2:3.0f}% | Total     {3:.0f}MB".format(gpu.memoryFree, gpu.memoryUsed, gpu.memoryUtil*100, gpu.memoryTotal))
     printm()
-    
-def printmultigraphs():
-    import seaborn as sns
-    sns.set_theme(style="darkgrid")
-    sns.set(rc={"figure.figsize":(20, 50)})
-    ax_lim = np.mean(running_loss_df.values, axis=0) * 1.1
 
-    fig, sub = plt.subplots(n_epochs // 2, 2)
-    for idx in range(n_epochs):
-      g = sns.lineplot(x=running_loss_df.index, y=idx, data=running_loss_df, ax=sub[idx // 2, idx % 2])
-      g.set_xlabel("Batches")
-      g.set_ylabel("Loss")
-      sub[idx // 2, idx % 2].set_ylim([0, ax_lim[idx]])
-      g.set_title(f"{idx} epoch")
-
-    plt.show(g)
     
